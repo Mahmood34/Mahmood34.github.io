0412e82549b0607c280e0b187a5a17cef1b8dd6e
Mahmood <49373537+Mahmood34@users.noreply.github.com>
2020-12-20 00:43:41 +0000

    Updated Theme and added extra Functionality
    Changed theme.
    Changed navigation buttons.
    Added/changed sections.
    Added relevant social media.
    Added an about me timeline.
    Implemented Figma embed frame.
    Implemented Sketchfab embed frame.
    Changed avatar icon.
    Changed divider icons.
    Added download link to CV.
    Made minor tweaks to visuals.
    Changed style sheet.

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 71)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+    $.ajax({ url: 'https://sketchfab.com/mahmood34/models', success: function(data) 
+    { 
+      alert(data); 
+      
+    } });
+
+  })(jQuery); // End of use strict
+  
+  function figmaClick(id) {
+  if(document.getElementById(id).getAttribute('src') == "")
+    {
+      document.getElementById(id).src = "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fw01PqDwjblCBptR52Yng7g%2FAmazon-Redesign%3Fnode-id%3D1%253A2%26scaling%3Dscale-down";
+    }
+    
+  }
